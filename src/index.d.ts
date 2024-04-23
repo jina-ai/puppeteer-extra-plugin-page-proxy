31e147d66ab74e0c8078d9badc3c2f7462cf37d1
yanlong.wang <yanlong.wang@naiver.org>
2024-04-23 19:42:37 +0800

    wip

--- src/index.d.ts
+++ src/index.d.ts
@@ -13,19 +13,65 @@ export = useProxy;
  */
 declare function useProxy(page: object, proxy: string | object): Promise<any>;
 declare namespace useProxy {
-/**
- * **Request data from a lookupservice.**
- *
- * **Example:**
- * ```javascript
- * await useProxy(page, proxy);
- * const data = await useProxy.lookup(page);
- * console.log(data.ip);
- * ```
- * @param page 'Page' object to execute the request on.
- * @param lookupServiceUrl External lookup service to request data from. Fetches data from `api64.ipify.org` by default.
- * @param isJSON Whether to JSON.parse the received response. Defaults to `true`.
- * @param timeout Time in milliseconds after which the request times out. Defaults to `30000` ms.
- */
+	/**
+	 * **Request data from a lookupservice.**
+	 *
+	 * **Example:**
+	 * ```javascript
+	 * await useProxy(page, proxy);
+	 * const data = await useProxy.lookup(page);
+	 * console.log(data.ip);
+	 * ```
+	 * @param page 'Page' object to execute the request on.
+	 * @param lookupServiceUrl External lookup service to request data from. Fetches data from `api64.ipify.org` by default.
+	 * @param isJSON Whether to JSON.parse the received response. Defaults to `true`.
+	 * @param timeout Time in milliseconds after which the request times out. Defaults to `30000` ms.
+	 */
 	function lookup(page: object, lookupServiceUrl?: string, isJSON?: boolean, timeout?: number | string): Promise<any>;
+}
+
+
+import { PuppeteerExtraPlugin } from 'puppeteer-extra-plugin';
+import { HTTPRequest } from 'puppeteer';
+import { Response as GotResponse, Options as GotOptions } from 'got';
+
+declare export function getProxiedResponse(request: HTTPRequest, proxy: string, overrides?: {
+	url?: GotOptions['url'];
+	method?: GotOptions['method'];
+	postData?: GotOptions['body'];
+	headers?: GotOptions['headers'];
+}): Promise<{
+	status: GotResponse['statusCode'];
+	headers: GotResponse['headers'];
+	body: GotResponse['body'];
+}>;
+
+declare interface PuppeteerPageProxyOptions {
+	onlyNavigation?: boolean;
+	interceptResolutionPriority?: number;
+}
+
+type PageProxyPluginPageAdditions = {
+	useProxy: (proxyUrl: string | null, options?: PuppeteerPageProxyOptions) => Promise<void>;
+};
+
+declare class PuppeteerPageProxyPlugin extends PuppeteerExtraPlugin {
+	constructor(proxyUrl?: string, opts?: Partial<PuppeteerPageProxyOptions>)
+	get name(): string
+	get defaults(): PuppeteerPageProxyOptions & { proxyUrl?: string }
+	get proxyUrl(): boolean | undefined;
+	get onlyNavigation(): boolean | undefined;
+	get interceptResolutionPriority(): number | undefined;
+}
+
+declare const _default: (proxyUrl?: string, opts?: Partial<PuppeteerPageProxyOptions>) => PuppeteerPageProxyPlugin;
+
+export default _default;
+
+declare module 'puppeteer' {
+	interface Page extends PageProxyPluginPageAdditions { };
+}
+
+declare module 'puppeteer-core' {
+	interface Page extends PageProxyPluginPageAdditions { };
 }
---
+++ src/plugin.js
@@ -0,0 +1,188 @@
+'use strict'
+
+const { PuppeteerExtraPlugin } = require('puppeteer-extra-plugin');
+const getProxiedResponse = require('./core/proxy');
+
+/**
+ * Block resources (images, media, css, etc.) in puppeteer.
+ *
+ * Supports all resource types, blocking can be toggled dynamically.
+ *
+ * @param {Object} opts - Options
+ * @param {Set<string>} [opts.blockedTypes] - Specify which resourceTypes to block (by default none)
+ *
+ * @example
+ * const { DEFAULT_INTERCEPT_RESOLUTION_PRIORITY } = require('puppeteer')
+ * puppeteer.use(require('puppeteer-extra-plugin-block-resources')({
+ *   blockedTypes: new Set(['image', 'stylesheet']),
+ *   // Optionally enable Cooperative Mode for several request interceptors
+ *   interceptResolutionPriority: DEFAULT_INTERCEPT_RESOLUTION_PRIORITY
+ * }))
+ *
+ * //
+ * // and/or dynamically:
+ * //
+ *
+ * const blockResourcesPlugin = require('puppeteer-extra-plugin-block-resources')()
+ * puppeteer.use(blockResourcesPlugin)
+ *
+ * const browser = await puppeteer.launch({ headless: false })
+ * const page = await browser.newPage()
+ *
+ * blockResourcesPlugin.blockedTypes.add('image')
+ * await page.goto('http://www.msn.com/', {waitUntil: 'domcontentloaded'})
+ *
+ * blockResourcesPlugin.blockedTypes.add('stylesheet')
+ * blockResourcesPlugin.blockedTypes.add('other') // e.g. favicon
+ * await page.goto('http://news.ycombinator.com', {waitUntil: 'domcontentloaded'})
+ *
+ * blockResourcesPlugin.blockedTypes.delete('stylesheet')
+ * blockResourcesPlugin.blockedTypes.delete('other')
+ * blockResourcesPlugin.blockedTypes.add('media')
+ * blockResourcesPlugin.blockedTypes.add('script')
+ * await page.goto('http://www.youtube.com', {waitUntil: 'domcontentloaded'})
+ */
+class PuppeteerPageProxyPlugin extends PuppeteerExtraPlugin {
+    constructor(proxyUrl, opts = {}) {
+        super({ ...opts, proxyUrl });
+
+        this.proxyCfgMap = new WeakMap();
+        this.reqHdlRegSet = new WeakSet();
+    }
+
+    /**
+     * @override
+     */
+    get name() {
+        return 'page-proxy';
+    }
+
+    /**
+     * @override
+     */
+    get defaults() {
+        return {
+            proxyUrl: undefined,
+            interceptResolutionPriority: undefined,
+            onlyNavigation: undefined,
+        };
+    }
+
+    /**
+     * Get global proxy url.
+     *
+     * @type {string} - The global proxy url.
+     */
+    get proxyUrl() {
+        return this.opts.proxyUrl;
+    }
+
+    /**
+     * Get global onlyNavigation flag.
+     *
+     * @type {boolean} - Whether to only proxy navigation requests.
+     */
+    get onlyNavigation() {
+        return this.opts.onlyNavigation;
+    }
+
+    /**
+     * Get the global request interception resolution priority.
+     *
+     * Priority for Cooperative Intercept Mode can be configured either through `opts` or by modifying this property.
+     *
+     * @type {number} - A number for the request interception resolution priority.
+     */
+    get interceptResolutionPriority() {
+        return this.opts.interceptResolutionPriority;
+    }
+
+    /**
+     * @private
+     */
+    async onRequest(page, request) {
+        // Requests are immediately handled if not using Cooperative Intercept Mode
+        const alreadyHandled = request.isInterceptResolutionHandled
+            ? request.isInterceptResolutionHandled()
+            : true;
+
+        this.debug('onRequest', {
+            type,
+            shouldBlock,
+            alreadyHandled
+        });
+
+        if (alreadyHandled) {
+            return;
+        }
+
+        const {
+            proxy: localProxyUrl,
+            onlyNavigation: localOnlyNavigation,
+            interceptResolutionPriority: localInterceptResolutionPriority
+        } = this.proxyCfgMap.get(page);
+
+        const interceptResolutionPriority = localInterceptResolutionPriority || this.interceptResolutionPriority;
+        const onlyNavigation = localOnlyNavigation || this.onlyNavigation;
+
+        do {
+            if (!localProxyUrl && localProxyUrl !== undefined) {
+                break;
+            }
+
+            const proxyUrl = localProxyUrl || this.proxyUrl;
+            if (!proxyUrl) {
+                break;
+            }
+            if (onlyNavigation && !request.isNavigationRequest()) {
+                break;
+            }
+
+            try {
+                const respondWith = await getProxiedResponse(request, proxyUrl);
+
+                return request.respond(respondWith, interceptResolutionPriority);
+            } catch (err) {
+                this.debug('onProxyError', { error: err, proxy: localProxyUrl });
+                return request.abort('failed', interceptResolutionPriority);
+            }
+
+        } while (false)
+
+        const continueArgs = request.continueRequestOverrides
+            ? [request.continueRequestOverrides(), interceptResolutionPriority]
+            : [];
+
+        return request.continue(...continueArgs);
+    }
+
+    /**
+     * @private
+     * @override
+     */
+    async onPageCreated(page) {
+        this.debug('onPageCreated', { proxy: this.proxyUrl });
+
+        page.useProxy = (proxyUrl, opts = {}) => {
+            this.proxyCfgMap.set(page, {
+                // ...this.proxyCfgMap.get(page),
+                ...opts,
+                proxy: proxyUrl,
+            });
+            if (!this.reqHdlRegSet.has(page)) {
+                page.on('request', this.onRequest.bind(this, page));
+                this.reqHdlRegSet.add(page);
+            }
+            return page.setRequestInterception(true);
+        };
+
+        if (this.proxyUrl) {
+            await page.useProxy();
+        }
+    }
+}
+
+module.exports = function (pluginConfig) {
+    return new PuppeteerPageProxyPlugin(pluginConfig);
+}
+module.exports.getProxiedResponse = getProxiedResponse;
